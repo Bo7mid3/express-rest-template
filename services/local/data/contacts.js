34cb3be71a7d9f46369bdfb1c5a321e5739e4af7
bo7mid3 <ahmedbahloul2526@gmail.com>
2022-06-09 11:24:11 +0100

    register: select user type

--- services/local/data/contacts.js
+++ services/local/data/contacts.js
@@ -1,11 +1,11 @@
 const ChatHistory = require("../../../models/chat-history");
 
 const getContacts = async (userId) => {
-    const chats =  await ChatHistory.aggregate([{$lookup: { from: "users", localField: "client", foreignField: "_id", as: "client"}}, {$match: {repairMan: userId}}]);
+    const chats =  await ChatHistory.aggregate([{$lookup: { from: "users", localField: "client", foreignField: "_id", as: "user"}}, {$match: {repairMan: userId}}]);
     const contacts = [];
     for(let chat of chats) {
-        delete chat.client[0].password;
-        contacts.push({ client: chat.client[0] ,lastMsg: chat.history[chat.history.length-1] });
+        delete chat.user[0].password;
+        contacts.push({ user: chat.user[0] ,lastMsg: chat.history[chat.history.length-1] });
     }
     return contacts;
 }
